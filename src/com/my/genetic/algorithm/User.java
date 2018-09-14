26a164abaf9c19123a4fa6ab8af13a37ce44ddf1
马焱 <yan.ma@southgis.com>
2018-09-14 14:38:33 +0800

    add param start so that user can choose start point

--- src/com/my/genetic/algorithm/User.java
+++ src/com/my/genetic/algorithm/User.java
@@ -19,28 +19,47 @@ public class User {
 		return this.current_path;
 	}
 
+	public void printPath() {
+		System.out.println(Arrays.toString(this.current_path));
+	}
+
 	/**
 	 * 重新规划路线
 	 * 
 	 * @param c_path 当前的规划路线
-	 * @param v_num 走过了几个点
+	 * @param v_num  走过了几个点
+	 * @param start  选择出发点
 	 */
-	public void setPath(int[] c_path, int v_num) {
-		int[] user_pool = new int[c_path.length - v_num];
-		int index = 0;
-		for (int i = v_num; i < c_path.length; i++) {
-			user_pool[index] = c_path[i];
-			index++;
+	public void setPath(int[] c_path, int v_num, int start) {
+		int index0 = v_num; // 用于出发点检测
+		boolean start_exists = false;
+		int index1 = 0; // 用于新任务点存储
+		for (int i = v_num; i < c_path.length; i++) {  // 遍历当前路线，找出出发点所在位置
+			if (c_path[i] == start) {
+				start_exists = true;
+				break;
+			} else {
+				index0++;
+			}
 		}
-		System.out.println(Arrays.toString(user_pool));
+		if (!start_exists) {  // 如果不存在的话丢出错误
+			throw new IllegalArgumentException("The start point has been visited!");
+		}
+		int temp = c_path[index0];  //将出发点位置放在新路线的第一个位置
+		c_path[index0] = c_path[v_num];
+		c_path[v_num] = temp;
+		int[] user_pool = new int[c_path.length - v_num];
+		for (int j = v_num; j < c_path.length; j++) {
+			user_pool[index1] = c_path[j];
+			index1++;
+		}
 		Population pop = new Population();
 		Chromosome.setUserPool(user_pool);
-		
 		this.current_path = pop.start();
 	}
 
 	public static void main(String[] args) {
 		User u = new User();
-		u.setPath(u.getPath(), 2);
+		u.setPath(u.getPath(), 2, 5);
 	}
 }
